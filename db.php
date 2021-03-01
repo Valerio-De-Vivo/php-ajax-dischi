781be208b6eb260f5004a22a53e91cca82ae4eae
Valerio <valeriodevivo90@gmail.com>
2021-03-01 16:21:14 +0100

    Work on Main for display Vinyls

---
+++ db.php
@@ -0,0 +1,32 @@
+<?php 
+
+$vinili = [
+    [
+        'titolo'=>'The sound of 20th Season',
+        'produttore'=>'Sven Vath',
+        'copertina'=>'1.jpg',
+        'etichetta'=>'Cocoon',
+        'anno'=>'2019',
+    ],
+    [
+        'titolo'=>'999',
+        'produttore'=>'Franchino',
+        'copertina'=>'2.jpg',
+        'etichetta'=>'Metempsicosi',
+        'anno'=>'1999',
+    ],
+    [
+        'titolo'=>'Human after all',
+        'produttore'=>'Daft Punk',
+        'copertina'=>'3.jpg',
+        'etichetta'=>'Daft',
+        'anno'=>'2005',
+    ],
+    [
+        'titolo'=>'Customer is King',
+        'produttore'=>'Solomun',
+        'copertina'=>'4.jpg',
+        'etichetta'=>'Diynamic Music',
+        'anno'=>'2018',
+    ],
+];
